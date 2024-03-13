afa9d656cb0bb3656da9fcabec4cd9563dfeb249
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-13 15:32:01 +0000

    Solved 1.3

--- part1/courseinfo/frontend/src/Part.jsx
+++ part1/courseinfo/frontend/src/Part.jsx
@@ -1,7 +1,7 @@
 export default function Part(props) {
     return (
         <p>
-            {props.part} {props.exercises}
+            {props.part.name} {props.part.exercises}
         </p>
     )
 }
