b655f97ea394596176dd679417be50a18e835c9f
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-13 15:36:17 +0000

    Solved 1.5

--- part1/courseinfo/frontend/src/Content.jsx
+++ part1/courseinfo/frontend/src/Content.jsx
@@ -2,7 +2,7 @@ import Part from './Part';
 
 export default function Content(props) {
 
-    const partComponents = props.parts.map(part => <Part part={part} />)
+    const partComponents = props.parts.map((part, index) => <Part part={part} key={index} />)
     return (
         <div>
             {partComponents}
