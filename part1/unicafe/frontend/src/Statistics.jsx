23f54cdb38f90bdc4193fdc1fbd8579c49a78708
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-13 15:51:31 +0000

    Solved 1.7

--- part1/unicafe/frontend/src/Statistics.jsx
+++ part1/unicafe/frontend/src/Statistics.jsx
@@ -1,6 +1,9 @@
 import Entry from './Entry';
 
 export default function(props) {
+    const total = props.good + props.neutral + props.bad;
+    const average = (1 * props.good + 0 * props.neutral -1 * props.bad) / total;
+    const positiveProportion = props.good / total;
     return (
         <div>
             <h1>statistics</h1>
@@ -8,6 +11,11 @@ export default function(props) {
                 <Entry name="good" votes={props.good} />
                 <Entry name="neutral" votes={props.neutral} />
                 <Entry name="bad" votes={props.bad} />
+            </div>
+            <div>
+                <p>all {total}</p>
+                <p>average {average}</p>
+                <p>positive {positiveProportion * 100}%</p>
             </div>
         </div>
     )
