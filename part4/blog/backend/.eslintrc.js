ed1952f6f55da76c04d77e12280e0b1857cd778e
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-14 17:23:57 +0000

    Solved 4.1

---
+++ part4/blog/backend/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'plugins': [
+    '@stylistic/js'
+  ],
+  'rules': {
+    '@stylistic/js/indent': [
+      'error',
+      2
+    ],
+    '@stylistic/js/linebreak-style': [
+      'error',
+      'windows'
+    ],
+    '@stylistic/js/quotes': [
+      'error',
+      'single'
+    ],
+    '@stylistic/js/semi': [
+      'error',
+      'always'
+    ],
+  }
+};
