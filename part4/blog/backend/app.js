cd20291d659720ffc4f053072a4e90c91202238a
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-14 17:36:15 +0000

    Solved 4.2

---
+++ part4/blog/backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const blogsRouter = require('./controllers/blogController');
+
+mongoose.connect(config.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(() => console.error('Error connecting to MongoDB'));
+
+app.use(cors());
+app.use(express.json());
+
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+
+module.exports = app;
