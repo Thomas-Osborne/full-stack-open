78fde5a8e61a6fce9a625e341e236a1e2e1af096
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-14 15:52:17 +0000

    Solved 3.21

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -10,102 +10,102 @@ const Person = require('./models/Person');
 app.use(express.static('dist'));
 app.use(express.json());
 
-const cors = require('cors')
-app.use(cors())
+const cors = require('cors');
+app.use(cors());
 
 app.use(morgan('tiny'));
-morgan.token('object', (req, res) => `${JSON.stringify(req.body)}`);
+morgan.token('object', req => `${JSON.stringify(req.body)}`);
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :object'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+  res.send('Hello World');
+});
 
 app.get('/api/persons', (req, res) => {
-    Person.find({ })
-        .then(person => {
-            res.json(person);
-        })
-})
+  Person.find({ })
+    .then(person => {
+      res.json(person);
+    });
+});
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id;
-    Person.findOne({ _id: id })
-        .then(person => res.json(person))
-        .catch(error => next(error));
-})
+  const id = req.params.id;
+  Person.findOne({ _id: id })
+    .then(person => res.json(person))
+    .catch(error => next(error));
+});
 
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body;
+  const body = req.body;
 
-    if (!body) {
-        return res.status(400).json({ 
-            error: 'body missing' 
-        })
-    }
+  if (!body) {
+    return res.status(400).json({ 
+      error: 'body missing' 
+    });
+  }
 
-    if (!body.name || !body.number) {
-        return res.status(400).json({
-            error: 'need name and number'
-        })
-    }
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'need name and number'
+    });
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  });
   
-    person.save()
-        .then(savedPerson => {
-            res.json(savedPerson);
-        })
-        .catch(error => next(error))
-})
+  person.save()
+    .then(savedPerson => {
+      res.json(savedPerson);
+    })
+    .catch(error => next(error));
+});
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
     
-    const body = req.body;
-    Person.findOneAndUpdate({ _id: id }, {name: body.name, number: body.number}, {new: true}) // new means the updated document is returned 
-        .then(person => res.json(person))
-        .catch(error => next(error));
-})
+  const body = req.body;
+  Person.findOneAndUpdate({ _id: id }, {name: body.name, number: body.number}, {new: true}) // new means the updated document is returned 
+    .then(person => res.json(person))
+    .catch(error => next(error));
+});
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Person.findOneAndDelete({ _id: id }) // new means the updated document is returned 
-        .then(result => res.status(204).end())
-        .catch(error => next(error));
-})
+  Person.findOneAndDelete({ _id: id }) // new means the updated document is returned 
+    .then(res.status(204).end())
+    .catch(error => next(error));
+});
 
 app.get('/info', (req, res, next) => {
-    Person.countDocuments({})
-        .then(result => {
-            const time = new Date();
-            let info = `<p>Phonebook has info for ${result} people</p><p>${time}</p>`
-            res.send(info)
-        })
-        .catch(error => next(error));
-  })
+  Person.countDocuments({})
+    .then(result => {
+      const time = new Date();
+      let info = `<p>Phonebook has info for ${result} people</p><p>${time}</p>`;
+      res.send(info);
+    })
+    .catch(error => next(error));
+});
 
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message);
   
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
   
-    next(error)
-}
+  next(error);
+};
   
 // this has to be the last loaded middleware, also all the routes should be registered before this!
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
--- part3/phonebook/backend/models/Person.js
+++ part3/phonebook/backend/models/Person.js
@@ -2,25 +2,25 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI)
-    .then(result => console.log('Connected to MongoDB'))
-    .catch(error => console.error('Error connecting to MongoDB'));
+  .then(console.log('Connected to MongoDB'))
+  .catch(console.error('Error connecting to MongoDB'));
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
-    },
-    number: {
-        type: String,
-        minlength: 8,
-        validate: {
-            validator: function(v) {
-                return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `A phone number should be of at the form A-B, where A consists of 2 or 3 numbers and B consists of numbers.`
-        }
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: 'A phone number should be of at the form A-B, where A consists of 2 or 3 numbers and B consists of numbers.'
     }
-})
+  }
+});
 
 module.exports = mongoose.model('Person', personSchema);
