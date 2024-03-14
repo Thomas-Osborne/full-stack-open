04b34a16102541c95565fa4ddfa65b0b04f2878b
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-14 15:22:49 +0000

    Solved 3.20

--- part3/phonebook/backend/models/Person.js
+++ part3/phonebook/backend/models/Person.js
@@ -11,7 +11,16 @@ const personSchema = new mongoose.Schema({
         minlength: 3,
         required: true,
     },
-    number: String,
+    number: {
+        type: String,
+        minlength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `A phone number should be of at the form A-B, where A consists of 2 or 3 numbers and B consists of numbers.`
+        }
+    }
 })
 
 module.exports = mongoose.model('Person', personSchema);
