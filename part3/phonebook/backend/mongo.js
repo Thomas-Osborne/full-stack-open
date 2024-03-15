01a84dc6c871b4fbb51ea5f07a726b23c3245dff
Thomas-Osborne <141839530+Thomas-Osborne@users.noreply.github.com>
2024-03-15 13:55:39 +0000

    Solved 4.8

--- part3/phonebook/backend/mongo.js
+++ part3/phonebook/backend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-    console.log("Give password as argument");
-    process.exit(1);
+  console.log('Give password as argument');
+  process.exit(1);
 }
 
 const password =  process.argv[2];
@@ -14,9 +14,9 @@ mongoose.set('strictQuery', false);
 mongoose.connect(mongoUri);
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+  name: String,
+  number: String,
+});
 
 const Person = mongoose.model('Person', personSchema);
 
@@ -31,9 +31,9 @@ const Person = mongoose.model('Person', personSchema);
 // })
 
 Person.find({ }).then(result => {
-    console.log('phonebook:')
-    result.forEach(person => {
-        console.log(person.name, person.number)
-    })
-    mongoose.connection.close();
-})
+  console.log('phonebook:');
+  result.forEach(person => {
+    console.log(person.name, person.number);
+  });
+  mongoose.connection.close();
+});
